dc844fa586f6579abe42ea4816dbabb7b888885e
Sarseth <elune90@gmail.com>
2019-12-29 19:41:00 +0100

    Working scan for type of appearance

--- src/issue/issuePageModel.js
+++ src/issue/issuePageModel.js
@@ -1,11 +1,10 @@
 const JQuery = require('jquery');
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 const containsComicsCategorySelector = ".page-header__categories-links > :contains('Comics')";
-const headerSelector = ".page-header__title";
 const imageSelector = "#templateimage .image-thumbnail > img";
 
-let IssuePageModel = function (baseUrl, issuePageWindow) {
-    this.baseUrl = baseUrl;
+let IssuePageModel = function (issuePageWindow, characterUrl) {
+    this.characterUrl = characterUrl;
     this.jQuery = new JQuery(issuePageWindow);
     this.keywords = issuePageWindow.window.document.getElementsByTagName("meta")["keywords"]["content"].split(",").map(item => item.trim());
     this.fullName = this.keywords.find(value => new RegExp(".+(\\d)+ (\\d)+$").test(value));
@@ -31,13 +30,13 @@ IssuePageModel.prototype.getIssueNo = function () {
 
 IssuePageModel.prototype.getImage = function () {
     try {
-        const elementWithImage = jQuery.find(selector)[0];
+        const elementWithImage = this.jQuery.find(imageSelector)[0];
         if (elementWithImage.attributes["data-src"]) {
             return elementWithImage.attributes["data-src"]["value"];
         }
         return elementWithImage.attributes["src"]["value"];
     } catch (error) {
-        console.error(`Error during finding element by ${selector}`);
+        console.error(`Error during finding element by ${imageSelector}`);
         throw error;
     }
 };
@@ -47,5 +46,45 @@ IssuePageModel.prototype.getPublishedDate = function () {
     const month = this.keywords.find(value => months.includes(value));
     return new Date(year, months.findIndex(value => value === month)).getTime();
 };
+
+IssuePageModel.prototype.getAppearanceType = function () {
+    return "DD";
+};
+
+IssuePageModel.prototype.getCharacterFocusType = function () {
+    const nextElementToCharacterTypeAppearance = this.jQuery.find(`[href="${this.characterUrl}"]`)[0].nextElementSibling;
+    if (!nextElementToCharacterTypeAppearance) {
+        return "";
+    }
+    const regex = /\(.+\)/
+    if (nextElementToCharacterTypeAppearance.nextElementSibling) {
+        return regex.exec(nextElementToCharacterTypeAppearance.nextElementSibling.innerHTML)[0];
+    }
+    return regex.exec(nextElementToCharacterTypeAppearance.innerHTML)[0];
+};
+
+IssuePageModel.prototype.getSubtitle = function () {
+
+};
+
+function resolveAppearances(jQuery, characterUrl) {
+    for (let i = 0; i < 10; i++) {
+        const startingHeader = jQuery.find(`#AppearingHeader${i}`)[0];
+        if (startingHeader) {
+            return findNextSiblingWithDesiredCharacter(startingHeader, characterUrl);
+        }
+    }
+}
+
+function findNextSiblingWithDesiredCharacter(elementToStartFrom, characterUrl) {
+    const nextElementSibling = elementToStartFrom.nextElementSibling;
+    if (!nextElementSibling) {
+        return undefined;
+    } else if (nextElementSibling.innerHTML.includes(characterUrl)) {
+        const spanElement = nextElementSibling.innerHTML;
+        return spanElement.substring();
+    }
+    return findNextSiblingWithDesiredCharacter(nextElementSibling, characterUrl);
+}
 
 module.exports = IssuePageModel;
