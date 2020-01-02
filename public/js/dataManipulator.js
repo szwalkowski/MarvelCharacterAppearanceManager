73af8be9a59b5b6be9c7f19c16513b3ba3596a30
Sarseth <elune90@gmail.com>
2020-01-02 06:51:09 +0100

    #2 when clicking on character button, we're displaying universes of it

--- public/js/dataManipulator.js
+++ public/js/dataManipulator.js
@@ -1,4 +1,6 @@
 const $characterListDiv = $('.character-list-options-buttons');
+const $universeListDiv = $('.universe-list-options-buttons');
+let pickedCharacterAlias;
 let charactersData;
 
 loadCharacters();
@@ -10,16 +12,25 @@ function loadCharacters() {
         async: true,
         success: (responseData) => {
             $characterListDiv.empty();
+            $universeListDiv.empty();
             charactersData = JSON.parse(responseData);
             charactersData.forEach(character => {
                 $characterListDiv.append(`<button id="${character.alias}" type="button">${character.alias.replace('_', ' ')}</button><br/>`);
                 $(`#${character.alias}`).click((btn) => {
-                    const universes = charactersData.find(character => character.alias === btn.target.id).universes;
+                    $universeListDiv.empty();
+                    pickedCharacterAlias = btn.target.id;
+                    showCharacterUniverses();
                 });
             });
         },
         error: (error) => {
             console.error(error);
         }
+    });
+}
+function showCharacterUniverses() {
+    const universes = charactersData.find(character => character.alias === pickedCharacterAlias).universes;
+    universes.forEach(universe => {
+        $universeListDiv.append(`<button id="${universe}" type="button">${universe}</button><br/>`);
     });
 }
