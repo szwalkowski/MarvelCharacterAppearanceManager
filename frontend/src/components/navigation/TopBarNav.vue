1c9d16cc20de79a8ec6d7b522af4b88e4651fc08
Sarseth <elune90@gmail.com>
2020-02-16 08:35:48 +0100

    #57: Singup login and hello info on nav bar

--- frontend/src/components/navigation/TopBarNav.vue
+++ frontend/src/components/navigation/TopBarNav.vue
@@ -29,6 +29,31 @@
           </div>
         </li>
       </ul>
+      <ul class="form-inline navbar-nav">
+        <template v-if="!userName">
+          <router-link tag="a" class="nav-link pr-sm-1" to="/log-in">
+            Log in
+          </router-link>
+          /
+          <router-link tag="a" class="nav-link pl-sm-1" to="/sign-up">
+            Sign up
+          </router-link>
+        </template>
+        <template v-else>
+          <router-link tag="a" class="nav-link pl-sm-1" to="/account">
+            Hello, {{ userName }}!
+          </router-link>
+        </template>
+      </ul>
     </div>
   </nav>
 </template>
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  computed: {
+    ...mapGetters(["userName"])
+  }
+};
+</script>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,7 @@ import VueSimpleAlert from "vue-simple-alert";
 import App from "./App.vue";
 import router from "./router";
 import axios from "axios";
+import store from "./store/store";
 
 Vue.config.productionTip = false;
 axios.defaults.baseURL = "http://localhost:3000";
@@ -11,5 +12,6 @@ Vue.use(VueSimpleAlert);
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount("#app");
