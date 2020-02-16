c8e0cc8dcfeb693c9316bdb7579eb9bf10a6287e
Sarseth <elune90@gmail.com>
2020-02-16 08:55:50 +0100

    #59: Created main page info

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,17 +1,22 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h2>Welcome to Marvel Character Appearance Manager!</h2>
+    <p>
+      Application is supposed to help you with reading Marvel Comics when
+      focusing certain characters.
+    </p>
+    <p>
+      Filter comics by those you read and appearance type if don't want to waste
+      time by minor appearances.
+    </p>
+    <p>
+      Data is based on
+      <a href="https://marvel.fandom.com/wiki">Marvel Database</a>
+      wiki content.
+    </p>
+    <p>All new issues should be added automatically every 24 hours.</p>
+    <p>
+      If you find something not updated or missing character, please contact me!
+    </p>
   </div>
 </template>
-
-<script>
-import HelloWorld from "@/components/HelloWorld";
-
-export default {
-  name: "home",
-  components: {
-    HelloWorld
-  }
-};
-</script>
